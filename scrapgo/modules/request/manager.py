23495aa4d2d6323f2e937709cb174056b5c656b2
HS Moon <pbr112@naver.com>
2019-03-16 20:21:55 +0900

    RequestsManager 내부 모듈 캐쉬드세션으로 교체

--- scrapgo/modules/request/manager.py
+++ scrapgo/modules/request/manager.py
@@ -16,25 +16,28 @@ class RequestsManager(object):
     def __init__(self,
                  headers=REQUEST_HEADER, cache_name=settings.CACHE_NAME,
                  cache_backend=settings.CACHE_BACKEND, cache_expiration=settings.CACHE_EXPIRATION,
-                 use_request_cache=True, use_session=True, **kwargs):
+                 use_request_cache=True, **kwargs):
         self.headers = headers
 
         if use_request_cache:
-            requests_cache.install_cache(
-                cache_name=cache_name, backend=cache_backend, expire_after=cache_expiration, **kwargs)
-        self.requests = requests
-        if use_session:
-            self.requests = requests.Session()
+            s = requests_cache.CachedSession(
+                cache_name=cache_name, backend=cache_backend, expire_after=cache_expiration)
+        else:
+            s = requests.Session()
+        self.requests = s
 
     def _get(self, url, cache=True, headers=None, **kwargs):
         headers = headers or self.headers
         print('Requestmanager._get:url', url)
         print('Requestmanager._get:headers', headers, '\n')
         if cache == True:
-            return self.requests.get(url, headers=headers, **kwargs)
-        with requests_cache.disabled():
-            print('manager.py:disable_cache', url)
-            return self.requests.get(url, headers=headers, **kwargs)
+            r = self.requests.get(url, headers=headers, **kwargs)
+        else:
+            with self.requests.cache_disabled():
+                print('manager.py:disable_cache', url)
+                r = self.requests.get(url, headers=headers, **kwargs)
+        r.raise_for_status()
+        return r
 
     def _post(self, url, headers=None, data=None, **kwargs):
         headers = headers or self.headers
