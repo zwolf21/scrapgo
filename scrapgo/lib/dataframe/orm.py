45a157262cf6c1af410be992b958279c8d1754b3
zwolf21 <pbr112@naver.com>
2019-05-28 02:33:12 +0900

    orm 모듈 테스트 및 버그 수정

--- scrapgo/lib/dataframe/orm.py
+++ scrapgo/lib/dataframe/orm.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlite3
 import pymysql
 from sqlalchemy import create_engine
 
@@ -22,24 +23,22 @@ QUERY_FORMAT = {
 
 class TableFrame(object):
 
-    def __init__(self, table_name, path_connect_info_jsonfile=None, **conn_info):
-        super().__init__(*args, **kwargs)
+    def __init__(self, path_connect_info_jsonfile=None, **conn_info):
         if path_connect_info_jsonfile:
             if get_file_extension(path_connect_info_jsonfile) == '.json':
                 conn_info = read_json(path_connect_info_jsonfile)
         self.con = self._get_db_connection(**conn_info)
-        self.table = table_name
 
     def __del__(self):
         self.con.close()
 
-    def _get_db_connection(self, **conn_info):
+    def _get_db_connection(self, **kwargs):
         db_backend = kwargs.get('backend', 'sqlite3')
         if db_backend == 'sqlite3':
-            con = sqlite3.connect(**conn_info)
+            con = sqlite3.connect(**kwargs)
         elif db_backend == 'mysql':
             con_str_fmt = "mysql+pymysql://{user}:{passwd}@{host}:{port}/{db}?charset={charset}"
-            con_str = con_str_fmt.format(**conn_info)
+            con_str = con_str_fmt.format(**kwargs)
             engin = create_engine(con_str, encoding='utf-8')
             con = engin.connect()
         else:
@@ -49,55 +48,61 @@ class TableFrame(object):
     def _get_now(self):
         return pd.Timestamp.now()
 
-    def _get_query(self, kind, **kwargs, query_formats=QUERY_FORMAT):
+    def _get_query(self, kind, table, query_formats=QUERY_FORMAT, **kwargs):
         fmt = query_formats[kind]
-        kwargs.setdefault('table', self.table_name)
-        return fmt.format(**kwargs)
+        return fmt.format(table=table, **kwargs)
 
     def get_dataframe(self, query, **kwargs):
         dataframe = pd.read_sql(query, self.con, **kwargs)
         return dataframe
 
-    def select(self, *columns, where=None, **kwargs):
-        columns = ','.join(columns) or '*'
+    def select(self, table, columns=None, where=None):
+        if columns is None:
+            columns = '*'
+        else:
+            columns = ','.join(columns)
         if where is None:
-            query = self._get_query('select', columns=columns, **kwargs)
+            query = self._get_query('select', table, columns=columns)
         else:
             query = self._get_query(
-                'select-where', columns=columns, where=where, **kwargs
+                'select-where', table, columns=columns, where=where
             )
         df = self.get_dataframe(query)
         return df
 
-    def max(self, column, where=None, **kwargs):
+    def max(self, table, column, where=None, **kwargs):
         if where is None:
-            query = self._get_query('max', column=column, **kwargs)
+            query = self._get_query('max', table, column=column, **kwargs)
         else:
             query = self._get_query(
-                'max-where', column=column, where=where, **kwargs
+                'max-where', table,
+                column=column, where=where, **kwargs
             )
         df = self.get_dataframe(query)
         return df.loc[0, f"MAX({column})"]
 
-    def min(self, column, where=None, **kwargs):
+    def min(self, table, column, where=None, **kwargs):
         if where is None:
-            query = self._get_query('min', column=column, **kwargs)
+            query = self._get_query('min', table, column=column, **kwargs)
         else:
             query = self._get_query(
-                'min-where', column=column, where=where, **kwargs
+                'min-where', table,
+                column=column, where=where, **kwargs
             )
         df = self.get_dataframe(query)
-        return df.loc[0, f"Min({column})"]
+        return df.loc[0, f"MIN({column})"]
 
-    def count(self, where=None, **kwargs):
+    def count(self, table, where=None, **kwargs):
         if where is None:
-            query = self._get_query('count', **kwargs)
+            query = self._get_query('count', table, **kwargs)
         else:
-            query = self._get_query('count', where=where, **kwargs)
+            query = self._get_query(
+                'count-where', table, where=where, **kwargs
+            )
         df = self.get_dataframe(query)
         return df.loc[0, "COUNT(*)"]
 
-    def insert_dataframe(self, dataframe, uniques, renames=None, updated=None, created=None, if_exists='append', **kwargs):
+    def insert_dataframe(self, dataframe, table, uniques, renames=None, updated=None, created=None, if_exists='append', **kwargs):
         if renames is not None:
             dataframe = dataframe.rename(columns=renames)
 
@@ -105,13 +110,16 @@ class TableFrame(object):
         if if_exists == 'append':
             dataframe = get_difference_from(dataframe, table, uniques)
 
+        if dataframe.empty:
+            return
+
         if updated is not None:
             dataframe[updated] = self._get_now()
         if created is not None:
             dataframe[created] = self._get_now()
 
         dataframe.to_sql(
-            self.table_name, self.con,
+            table, self.con,
             if_exists=if_exists,
             index=False
         )
